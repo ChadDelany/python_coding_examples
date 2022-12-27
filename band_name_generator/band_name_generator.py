175ef1bfbdf9f510b0448096e0014181ec8497e0
Chad Delany <info@chaddelany.com>
2022-12-27 10:13:22 -0800

    band name generator

---
+++ band_name_generator/band_name_generator.py
@@ -0,0 +1,7 @@
+# 100 Days of Code - Udemy
+# Day 1 - Band Name Generator
+
+print('This is a Band Name Generator.')
+town = input("What is the name of the town you grew up in?\n")
+pet = input('What is the name of your childhood pet?\n')
+print('Your band name could be ' + town + ' ' + pet + '.')
