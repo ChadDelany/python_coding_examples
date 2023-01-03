af22b2f1e9b304faf3221a872e2ee87ffd3b4178
Chad Delany <info@chaddelany.com>
2023-01-02 17:33:05 -0800

    removed comments for clear function

--- basic_examples/09_silent_auction.py
+++ basic_examples/09_silent_auction.py
@@ -32,13 +32,3 @@ while not bidding_finished:
         find_highest_bidder(bids)
     elif should_continue == "yes":
         clear()
-
-"""
-FAQ: My console doesn't clear()
-
-This will happen if you’re using an IDE other than replit. 
-I’ll cover how to use PyCharm in Day 15. That said, you can write your own clear() function or configure your IDE like so: 
-
-https://www.udemy.com/course/100-days-of-code/learn/lecture/19279420#questions/16084076
-
-"""
