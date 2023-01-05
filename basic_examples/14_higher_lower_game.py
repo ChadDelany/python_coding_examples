f66a32790334a7f55416e6f21031b2ca391ccef2
Chad Delany <info@chaddelany.com>
2023-01-05 11:04:03 -0800

    higher lower game

--- basic_examples/14_higher_lower_game.py
+++ basic_examples/14_higher_lower_game.py
@@ -1,6 +1,35 @@
 # Higher Lower Game
+from random import sample, randint
 from art_higher_lower import logo
 from data_higher_lower import data
 
 print(logo)
-data
+# print(len(data))
+# print(sample(range(len(data)), k=2))
+# print(data[[sample(range(len(data)), k=2)]])
+
+# select = sample(range(len(data)), k=2)
+# account0 = data[select[0]]
+# account1 = data[select[1]]
+# print([account0, account1])
+
+
+def select(previous=0):
+    if previous == 0:
+        # Select two accounts that are different
+        select = sample(range(len(data)), k=2)
+        account0 = data[select[0]]
+        account1 = data[select[1]]
+        return [account0, account1]
+    else:
+        # Select one new account
+        select = data[previous]
+        while select == data[previous]:
+            select = randint(range(len(data)))
+        account1 = data[select]
+        return [previous, account1]
+def compare():
+    return
+
+def game():
+    return
