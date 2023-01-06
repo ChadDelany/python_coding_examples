9cfbb7f02a959ddc90381e56f938c76454484c2e
Chad Delany <info@chaddelany.com>
2023-01-05 17:58:02 -0800

    updated higher lower game

--- basic_examples/14_higher_lower_game.py
+++ basic_examples/14_higher_lower_game.py
@@ -1,35 +1,66 @@
 # Higher Lower Game
-from random import sample, randint
-from art_higher_lower import logo
 from data_higher_lower import data
-
-print(logo)
-# print(len(data))
-# print(sample(range(len(data)), k=2))
-# print(data[[sample(range(len(data)), k=2)]])
-
-# select = sample(range(len(data)), k=2)
-# account0 = data[select[0]]
-# account1 = data[select[1]]
-# print([account0, account1])
+import random
+from art_higher_lower import logo, vs
+# clear function for Linux
+import os
+clear = lambda: os.system('clear')
 
 
-def select(previous=0):
-    if previous == 0:
-        # Select two accounts that are different
-        select = sample(range(len(data)), k=2)
-        account0 = data[select[0]]
-        account1 = data[select[1]]
-        return [account0, account1]
+def get_random_account():
+    """Get data from random account"""
+    return random.choice(data)
+
+
+def format_data(account):
+    """Format account into printable format: name, description and country"""
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Checks followers against user's guess
+    and returns True if they got it right.
+    Or False if they got it wrong."""
+    if a_followers > b_followers:
+        return guess == "a"
     else:
-        # Select one new account
-        select = data[previous]
-        while select == data[previous]:
-            select = randint(range(len(data)))
-        account1 = data[select]
-        return [previous, account1]
-def compare():
-    return
+        return guess == "b"
+
 
 def game():
-    return
+    print(logo)
+    score = 0
+    game_should_continue = True
+    account_a = get_random_account()
+    account_b = get_random_account()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_account()
+
+        while account_a == account_b:
+            account_b = get_random_account()
+
+        print(f"Compare A: {format_data(account_a)}.")
+        print(vs)
+        print(f"Against B: {format_data(account_b)}.")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_count = account_a["follower_count"]
+        b_follower_count = account_b["follower_count"]
+        is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+        clear()
+        print(logo)
+        if is_correct:
+            score += 1
+            print(f"You're right! Current score: {score}.")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
+
+
+game()
