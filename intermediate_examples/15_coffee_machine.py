e93554e10ffc19282ed3ad6cdf52d6528fabd0f8
Chad Delany <info@chaddelany.com>
2023-02-13 09:59:15 -0800

    transaction successful function

--- intermediate_examples/15_coffee_machine.py
+++ intermediate_examples/15_coffee_machine.py
@@ -39,7 +39,7 @@ resources = {
 def is_resource_sufficient(order_ingredients):
     """Determines if there are enough resources to make the drink."""
     for item in order_ingredients:
-        if order_ingredients[item] >= resources[item]
+        if order_ingredients[item] >= resources[item]:
             print(f'Sorry there is not enough water {item}.')
             return False
     return True
@@ -53,6 +53,20 @@ def process_coins():
     total += int(input('How many nickles?: ')) * 0.05
     total += int(input('How many pennies?: ')) * 0.01
     return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient."""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f'Here is ${change} in change.')
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money.  Money refunded.")
+        return False
+
 
 
 is_on = True
