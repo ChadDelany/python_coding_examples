82b19f9d2bf48a4548237781bab3659d8f2832ca
Chad Delany <info@chaddelany.com>
2023-02-13 09:41:25 -0800

    input parameters for coffee machine

--- intermediate_examples/15_coffee_machine.py
+++ intermediate_examples/15_coffee_machine.py
@@ -27,10 +27,26 @@ MENU = {
     }
 }
 
+profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+is_on = True
+
+while is_on:
+    choice = input('What would you like? (espresso/latte/cappuccino): ')
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        print(f'Water: {resources["water"]}ml')
+        print(f'Milk: {resources["milk"]}ml')
+        print(f'Coffee: {resources["coffee"]}g')
+        print(f'Money: {profit}')
+        
+
 
 # TODO: 1. Print report of all coffee machine resources.
