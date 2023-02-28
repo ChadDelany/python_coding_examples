e1965a47b851d2d5b56e3ea745f2c01457df4a61
Chad Delany <info@chaddelany.com>
2023-02-28 10:59:32 -0800

    question class & data reformat

--- intermediate_examples/17_quiz_game_OOP.py
+++ intermediate_examples/17_quiz_game_OOP.py
@@ -1,22 +1,26 @@
-# Modeling Instagram User
+# # Modeling Instagram User
+#
+# class User:
+#
+#     def __init__(self, user_id, username):
+#         self.id = user_id
+#         self.username = username
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User('001', 'chad')
+# user_2 = User('002', 'jack')
+#
+# user_1.follow(user_2)
+#
+# print(user_1.followers)
+# print(user_1.following)
 
-class User:
+from question_model import Question
+from quiz_game_data import question_data
 
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
-
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
-
-
-user_1 = User('001', 'chad')
-user_2 = User('002', 'jack')
-
-user_1.follow(user_2)
-
-print(user_1.followers)
-print(user_1.following)
