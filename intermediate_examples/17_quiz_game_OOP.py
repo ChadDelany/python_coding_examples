51271ab995aa5e25c82cdccac2752babbff9828e
Chad Delany <info@chaddelany.com>
2023-03-01 08:41:19 -0800

    created question_bank list

--- intermediate_examples/17_quiz_game_OOP.py
+++ intermediate_examples/17_quiz_game_OOP.py
@@ -24,3 +24,10 @@
 from question_model import Question
 from quiz_game_data import question_data
 
+question_bank = []
+for question in question_data:
+    question_text = question['text']
+    question_answer = question['answer']
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
