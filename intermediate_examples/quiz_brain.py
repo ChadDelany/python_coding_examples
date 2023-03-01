1d162e82c5c979b2e88ff633d2887d75888cc252
Chad Delany <info@chaddelany.com>
2023-03-01 08:51:46 -0800

    removed duplicate main file

--- intermediate_examples/quiz_brain.py
+++ intermediate_examples/quiz_brain.py
@@ -9,4 +9,5 @@ class QuizBrain:
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
+        self.question_number += 1
         input(f'Q.{self.question_number}: {current_question.text} (True/False): ')
