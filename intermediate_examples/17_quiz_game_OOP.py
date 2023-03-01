52a96b85e4aa7b772d44fa3ec6fcf780e53b70c9
Chad Delany <info@chaddelany.com>
2023-03-01 08:49:34 -0800

    updated QuizBrain class

--- intermediate_examples/17_quiz_game_OOP.py
+++ intermediate_examples/17_quiz_game_OOP.py
@@ -23,6 +23,7 @@
 
 from question_model import Question
 from quiz_game_data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
@@ -31,3 +32,5 @@ for question in question_data:
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
