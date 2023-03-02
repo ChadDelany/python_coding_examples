b8d59090141a6fe01f1da77c4389c138cd36f418
Chad Delany <info@chaddelany.com>
2023-03-02 10:50:56 -0800

    generate a spirograph

--- 18_turtle_GUI/main.py
+++ 18_turtle_GUI/main.py
@@ -1,32 +1,32 @@
-# '''Turtle GUI project'''
+# """Turtle GUI project"""
 # from turtle import Turtle, Screen
 # import random
 #
 # tim = Turtle()
 
-# '''Draw a Red Turtle, Move Forward, and Turn Right'''
+# """Draw a Red Turtle, Move Forward, and Turn Right"""
 # timmy_the_turtle.shape('turtle')
 # timmy_the_turtle.color('red')
 # timmy_the_turtle.forward(100)
 # timmy_the_turtle.right(90)
 
-# '''Draw a Square'''
+# """Draw a Square"""
 # for _ in range(4):
 #     tim.forward(100)
 #     tim.left(90)
 
-# '''Import a new non-standard library'''
+# """Import a new non-standard library"""
 # import heroes
 # print(heroes.gen())
 
-# '''Draw a Dashed Line.'''
+# """Draw a Dashed Line"""
 # for _ in range(15):
 #     tim.forward(10)
 #     tim.penup()
 #     tim.forward(10)
 #     tim.pendown()
 
-# '''Initial nested for loops for nested polygons (see next solution)'''
+# """Initial nested for loops for nested polygons (see next solution)"""
 # for num_sides in range(3,11):
 #     for sides in range(num_sides):
 #         angle = 360 / sides
@@ -49,7 +49,34 @@
 #     tim.color(random.choice(colors))
 #     draw_shape(shape_side_n)
 
-"""Generate a Random Walk"""
+# """Generate a Random Walk"""
+# import turtle as t
+# import random
+#
+# tim = t.Turtle()
+# t.colormode(255)
+#
+#
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     color = (r, g, b)
+#     return color
+#
+#
+# directions = [0, 90, 180, 270]
+# num_cycles = 200
+# tim.pensize(15)
+# tim.speed('fastest')
+#
+# for _ in range(num_cycles):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+
+"""Generate a Spirograph"""
 import turtle as t
 import random
 
@@ -65,15 +92,10 @@ def random_color():
     return color
 
 
-directions = [0, 90, 180, 270]
-num_cycles = 200
-tim.pensize(15)
 tim.speed('fastest')
+tim.cirlce(100)
 
-for _ in range(num_cycles):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+
 
 screen = t.Screen()
 screen.exitonclick()
