e18bef97363e69048ef330c8d84c300bc82a95bc
Chad Delany <info@chaddelany.com>
2023-03-02 10:58:34 -0800

    spirograph function

--- 18_turtle_GUI/main.py
+++ 18_turtle_GUI/main.py
@@ -82,6 +82,7 @@ import random
 
 tim = t.Turtle()
 t.colormode(255)
+tim.speed('fastest')
 
 
 def random_color():
@@ -92,10 +93,14 @@ def random_color():
     return color
 
 
-tim.speed('fastest')
-tim.cirlce(100)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
 
+draw_spirograph(5)
 
 screen = t.Screen()
 screen.exitonclick()
