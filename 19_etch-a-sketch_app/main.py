2500c382273484453a58cb98b78407f027e21009
Chad Delany <info@chaddelany.com>
2023-03-03 09:26:27 -0800

    functions to allow navigation

---
+++ 19_etch-a-sketch_app/main.py
@@ -0,0 +1,36 @@
+# Etch-a-Sketch app
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear_screen():
+    tim.forward(10)
+
+
+screen.listen()
+screen.onkey(move_forwards, 'w')
+screen.onkey(move_backwards, 's')
+screen.onkey(turn_left, 'a')
+screen.onkey(turn_right, 'd')
+screen.exitonclick()
+
