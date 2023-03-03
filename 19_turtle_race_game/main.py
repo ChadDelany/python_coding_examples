3da54a79c0f46bc65448eb293298d3c578ce4dd0
Chad Delany <info@chaddelany.com>
2023-03-03 09:57:22 -0800

    conditional to determine winner

--- 19_turtle_race_game/main.py
+++ 19_turtle_race_game/main.py
@@ -2,18 +2,37 @@
 # Multiple Instances of the Turtle Object
 
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title='Make your bet', prompt='Which turtle will win the race? Enter a color: ')
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
 for turtle_index in range(0, 6):
-    tim = Turtle(shape='turtle')
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
+is_race_on = False
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost.  The {winning_color} turtle is the winner.")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
