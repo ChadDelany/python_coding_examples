6aff52d610a8d1903034e8b1308aaf350f668f88
Chad Delany <info@chaddelany.com>
2023-03-06 09:45:05 -0800

    fixed indentation on movement conditional methods

--- 20_snake_game/snake.py
+++ 20_snake_game/snake.py
@@ -37,13 +37,14 @@ class Snake:
 
     def down(self):
         if self.head.heading() != UP:
-        self.head.setheading(DOWN)
+            self.head.setheading(DOWN)
 
     def left(self):
         if self.head.heading() != RIGHT:
-        self.head.setheading(LEFT)
+            self.head.setheading(LEFT)
 
     def right(self):
         if self.head.heading() != LEFT:
-        self.head.setheading(RIGHT)
+            self.head.setheading(RIGHT)
+
 
