a10a7c95ba2fd27eb353da25b27f9738a79e60a0
Chad Delany <info@chaddelany.com>
2023-03-06 09:06:59 -0800

    snake movement in unison

--- 20_snake_game/main.py
+++ 20_snake_game/main.py
@@ -22,16 +22,18 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
-# Show screen after 'snake' has been created.
-screen.update()
-
 # Move the 'snake' automatically
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 
