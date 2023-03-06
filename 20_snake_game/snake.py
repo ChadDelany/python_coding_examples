febea7fa1c4659dde7c08fe9e3bba413088ea3e1
Chad Delany <info@chaddelany.com>
2023-03-06 09:29:29 -0800

    updated Snake class

--- 20_snake_game/snake.py
+++ 20_snake_game/snake.py
@@ -1,27 +1,27 @@
 """Snake Class"""
-
 from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
 
 class Snake:
 
-    
-    # Constructing the 'snake'
-    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-    segments = []
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
 
-    for position in starting_positions:
-        new_segment = Turtle('square')
-        new_segment.color('white')
-        new_segment.penup()
-        new_segment.goto(position)
-        segments.append(new_segment)
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segments = Turtle('square')
+            new_segments.color('white')
+            new_segments.penup()
+            new_segments.goto(position)
+            self.segments.append(new_segments)
 
-    # Move end of snake to position of snake segment before it (inch-worm movement)
-    def self.move():
-        for seg_num in range(len(segments) - 1, 0, -1):
-            new_x = segments[seg_num - 1].xcor()
-            new_y = segments[seg_num - 1].ycor()
-            segments[seg_num].goto(new_x, new_y)
-
-        segments[0].forward(20)
-        segments[0].left(90)
+    def move(self):
+        # Move end of snake to position of snake segment before it (inch-worm movement)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
+        self.segments[0].left(90)
