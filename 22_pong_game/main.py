539a475ff68c6b601b6ae988dee40b1bcea480a6
Chad Delany <info@chaddelany.com>
2023-03-08 14:56:54 -0800

    setup screen, initial paddle design

---
+++ 22_pong_game/main.py
@@ -0,0 +1,42 @@
+"""Pong Game"""
+
+# Libraries
+from turtle import Screen, Turtle
+
+# Setup Screen
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width=800, height=600)
+screen.title('Pong')
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape('square')
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.color('white')
+paddle.penup()
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, 'Up')
+screen.onkey(go_down, 'Down')
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
+
+
+screen.exitonclick()
