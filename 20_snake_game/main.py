30ca10d6446b1c901b4d6630ebeea517c76b85cb
Chad Delany <info@chaddelany.com>
2023-03-08 09:46:06 -0800

    slicing for tail collision detection

--- 20_snake_game/main.py
+++ 20_snake_game/main.py
@@ -45,10 +45,8 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
