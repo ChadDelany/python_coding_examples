7df4f4f6e23a0dafb92515044bd7acd6757387e5
Chad Delany <info@chaddelany.com>
2023-03-08 09:27:02 -0800

    scoreboard.gameover() method

--- 20_snake_game/main.py
+++ 20_snake_game/main.py
@@ -38,6 +38,14 @@ while game_is_on:
         food.refresh()
         scoreboard.increase_score()
 
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail.
+    
+
 
 
 
--- 20_snake_game/scoreboard.py
+++ 20_snake_game/scoreboard.py
@@ -21,10 +21,16 @@ class Scoreboard(Turtle):
     def update_scoreboard(self):
         self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
 
+    def game_over(self):
+        self.goto(0, 0)
+        self.write('GAME OVER', align=ALIGNMENT, font=FONT)
+
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+
 
 
 
