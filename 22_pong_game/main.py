8bbcf7a110e4da94db1c25b2abb57c1641e7cec3
Chad Delany <info@chaddelany.com>
2023-03-09 08:42:18 -0800

    increased movement speed with each paddle bounce

--- 22_pong_game/main.py
+++ 22_pong_game/main.py
@@ -30,7 +30,7 @@ screen.onkey(l_paddle.go_down, 's')
 # Update screen while the game is running.
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
