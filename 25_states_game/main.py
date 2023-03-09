46f8c9ca7db46a2c0b8295027228a877619e0c47
Chad Delany <info@chaddelany.com>
2023-03-09 14:46:00 -0800

    read weather data with csv library

---
+++ 25_states_game/main.py
@@ -0,0 +1,12 @@
+
+import csv
+
+with open('weather_data.csv') as data_file:
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        if row[1] != 'temp':
+            temperatures.append(int(row[1]))
+
+    print(temperatures)
+    
