5c653738d7f81c6dd6e10c2f8ff893517edf8185
Chad Delany <info@chaddelany.com>
2023-03-08 16:29:23 -0800

    movement methods for paddle class

--- 22_pong_game/main.py
+++ 22_pong_game/main.py
@@ -15,8 +15,10 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, 'Up')
-screen.onkey(go_down, 'Down')
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
--- 22_pong_game/paddle.py
+++ 22_pong_game/paddle.py
@@ -4,19 +4,19 @@ from turtle import Turtle
 
 class Paddle(Turtle):
     
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape('square')
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.color('white')
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
 
-    def go_up():
+    def go_up(self):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
-    def go_down():
+    def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
 
