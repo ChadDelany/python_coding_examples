5f8456bc0f4de6ec67365043266fdf754dace686
Chad Delany <info@chaddelany.com>
2023-03-08 16:24:58 -0800

    constructed paddle class

--- 22_pong_game/main.py
+++ 22_pong_game/main.py
@@ -1,7 +1,8 @@
 """Pong Game"""
 
 # Libraries
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 # Setup Screen
 screen = Screen()
@@ -10,23 +11,8 @@ screen.setup(width=800, height=600)
 screen.title('Pong')
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape('square')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.color('white')
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
 screen.onkey(go_up, 'Up')
---
+++ 22_pong_game/paddle.py
@@ -0,0 +1,22 @@
+"""self Class"""
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color('white')
+        self.penup()
+        self.goto(350, 0)
+
+    def go_up():
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down():
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
