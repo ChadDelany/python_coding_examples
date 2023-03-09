c9771f3a88c544edc81d42a1396b5883a4229641
Chad Delany <info@chaddelany.com>
2023-03-09 14:32:53 -0800

    updated formatting

--- 24_mail_merge/main.py
+++ 24_mail_merge/main.py
@@ -11,7 +11,7 @@ with open('./Input/Names/invited_names.txt') as names_file:
 with open('./Input/Letters/starting_letter.txt') as letter_file:
     letter_contents = letter_file.read()
 
-    # Format list of names
+    # Format list of names, insert name into form letter
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
