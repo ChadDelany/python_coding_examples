c7793f6820b765dac71681dd5aafb97aa10d2aff
Chad Delany <info@chaddelany.com>
2023-03-09 09:32:12 -0800

    game over method

--- 23_turtle_crossing_game/scoreboard.py
+++ 23_turtle_crossing_game/scoreboard.py
@@ -25,4 +25,6 @@ class Scoreboard(Turtle):
         self.level += 1
         self.update_scoreboard()
 
-
+    def game_over(self):
+        self.goto(0, 0)
+        self.write('GAME OVER', align='center', font=FONT)
